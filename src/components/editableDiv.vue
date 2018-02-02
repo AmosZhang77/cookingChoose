9de8cb9f8fdca00cf1893d6e8bc28428662dff94
pinksun77 <moonyellow@126.com>
2018-02-02 13:05:10 +0800

    cook start

---
+++ src/components/editableDiv.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>荤菜:</div>
+    <div class="r">{{m}}</div>
+    <div>蔬菜:</div>
+    <div class="r">{{f}}</div>
+    <button @click="start">start</button>
+    <button @click="chooseM">选荤菜</button>
+    <button @click="chooseF">选蔬菜</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'cook-APP',
+      mL: [
+        {name: '土豆红萝卜牛肉'},
+        {name: '蒜头小炒牛肉'},
+        {name: '蒜头炒鸡肉'},
+        {name: '鸡汤'},
+        {name: '蒜头烧鲫鱼'},
+        {name: '蒜头烧黄鱼'},
+        {name: '蒜头烧虾'},
+      ],
+      fL: [
+        {name: '红菜苔'},
+        {name: '白菜肉丝'},
+        {name: '蒜苔肉丝'},
+        {name: '蒜苔肉丝'},
+        {name: '胡萝卜肉片'},
+        {name: '茼蒿菜'},
+        {name: '莴笋肉片'},
+        {name: '蒜泥生菜'},
+        {name: '塔菜'},
+        {name: '芹菜肉丝'},
+      ],
+      m: '',
+      f: '',
+    }
+  },
+  mounted: function () {
+    this.iM = 0
+    this.iF = 0
+  },
+  methods: {
+    start: function () {
+      if (this.T1) {
+        clearInterval(this.T1)
+      }
+      if (this.T2) {
+        clearInterval( this.T2)
+      }
+      this.T1 = setInterval(() => {
+        this.showM()
+      }, 100)
+      this.T2 = setInterval(() => {
+        this.showF()
+      }, 100)
+    },
+    chooseM: function () {
+      clearInterval( this.T1)
+    },
+    chooseF: function () {
+      clearInterval( this.T2)
+    },
+    showM: function () {
+      this.iM++
+      if (this.iM < this.mL.length) {
+        this.m = this.mL[this.iM].name
+      } else {
+        this.iM = 0
+        this.m = this.mL[this.iM].name
+      }
+    },
+    showF: function () {
+      this.iF++
+      if (this.iF < this.fL.length) {
+        this.f = this.fL[this.iF].name
+      } else {
+        this.iF = 0
+        this.f = this.fL[this.iF].name
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
+<style lang="scss" scoped >
+ .r{
+   height: 20px;
+   background: #dddddd;
+ }
+</style>
